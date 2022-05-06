83165981440a5cb5cdcf6d047b75daf05ca03dad
= <60619582+andreyszcr@users.noreply.github.com>
2022-05-05 22:01:31 -0600

    operators

---
+++ CapaDatos/Operadores.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Operadores
+    {
+        int Numero1;
+        int Numero2;
+        int resultado;
+        public Operadores()
+        {
+            Numero1 = 0;
+            Numero2 = 0;
+            resultado = 0;
+        }
+        
+
+
+
+
+       public void Suma()
+       {
+            resultado=Numero1 + Numero2;
+       }
+
+        public void Resta()
+        {
+            resultado = Numero1 - Numero2;
+        }
+
+        public void Dividir()
+        {
+            resultado = Numero1 / Numero2;
+        }
+
+        public void Multiplicar()
+        {
+            resultado = Numero1 * Numero2;
+        }
+
+
+    }
+}
--- Proyecto01/Program.cs
+++ Proyecto01/Program.cs
@@ -1,6 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CapaDatos;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 double precio= 399;
 
 Console.WriteLine(precio);
+
+Console.WriteLine("Digite el numero");
+var Operators = new Operadores();
+
+
+
